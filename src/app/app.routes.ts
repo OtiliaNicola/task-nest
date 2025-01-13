d02f180f22ad2726c54c3d27d7db1fa2ed5dd73b
OtiliaNicola <otilianicola94@gmail.com>
2025-01-13 16:02:35 +0100

    feat: othes chanfges

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'tasks',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/task-list/task-list.component').then(m => m.TaskListComponent)
+      },
+      {
+        path: 'new',
+        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
+      },
+      {
+        path: 'edit/:id',
+        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
