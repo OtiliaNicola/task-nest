1ad335e2b5abff5830bf43566f3d1e99afde927f
OtiliaNicola <otilianicola94@gmail.com>
2025-01-15 13:01:26 +0100

    fix: add local date es

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'es-ES' }
   ]
 };
