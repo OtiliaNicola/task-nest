308e32c9bcec132191f87e7f1969797c596e345a
OtiliaNicola <otilianicola94@gmail.com>
2025-01-16 12:24:50 +0100

    fix: add logo, coptright and link github

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { LogoComponent } from '../../shared/components/logo/logo.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, LogoComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
